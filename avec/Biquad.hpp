5bce05adbf173dd2d04d88f6a2c3c3129d6ac215
dario mambro <dario.mambro@gmail.com>
2020-01-25 15:57:30 +0100

    added methods to move the state and settings of the biquad filter from a channel to an other

--- avec/Biquad.hpp
+++ avec/Biquad.hpp
@@ -33,12 +33,40 @@ enum class BiquadFilterType
 };
 
 /**
- * A simple biquad filter working with VecBuffers.
+ * An interface for @see VecBiquadFilter that abstracts over the simd register
+ * size. The only method included in this interface are those to set and get the
+ * state and settings of the filter.
  */
-template<typename Vec>
-class VecBiquadFilter
+template<typename Scalar>
+class VecBiquadFilterInterface
 {
 public:
+  virtual void Reset(int channel) = 0;
+  virtual void Setup(int channel, bool reset) = 0;
+  virtual void SetFrequency(int channel, double value, bool update) = 0;
+  virtual void SetGain(int channel, double value, bool update) = 0;
+  virtual void SetQuality(int channel, double value, bool update) = 0;
+  virtual void SetBiquadFilterType(int channel,
+                                   BiquadFilterType value,
+                                   bool update) = 0;
+  virtual double GetFrequency(int channel) const = 0;
+  virtual double GetGain(int channel) const = 0;
+  virtual double GetQuality(int channel) const = 0;
+  virtual BiquadFilterType GetBiquadFilterType(int channel) const = 0;
+  virtual void SetState(int channel, Scalar state0, Scalar state1) = 0;
+  virtual void GetState(int channel, Scalar& state0, Scalar& state1) const = 0;
+};
+
+/**
+ * A simple biquad filter working with VecBuffers.
+ */
+template<class Vec>
+class VecBiquadFilter final
+  : public VecBiquadFilterInterface<typename ScalarTypes<Vec>::Scalar>
+{
+public:
+  using Scalar = typename ScalarTypes<Vec>::Scalar;
+
   /**
    * Constructor.
    * @param filterType_ the type of filter (low pass, high shelf, ... @see
@@ -101,47 +129,76 @@ public:
   }
 
   /**
+   * Resets the state of the filter for the specified channel, as if it was
+   * processing silence from an eternity.
+   * @param channel the channel to reset
+   */
+  void Reset(int channel) override
+  {
+    buffer[5][channel] = 0.0;
+    buffer[6][channel] = 0.0;
+  }
+
+  /**
    * Sets the cutoff frequency on a specific channel
    * @param channel the channel on which to change the cutoff frequency
    * @param value the new cutoff frequency
+   * @param update if true, as it is by default, the new filter coefficients
+   * will be computed. Otherwise they will not be computed.
    */
-  void SetFrequency(int channel, double value)
+  void SetFrequency(int channel, double value, bool update = true) override
   {
     frequency[channel] = value;
-    Setup();
+    if (update) {
+      Setup(channel);
+    }
   }
 
   /**
    * Sets the gain on a specific channel
    * @param channel the channel on which to change the gain
    * @param value the new gain
+   * @param update if true, as it is by default, the new filter coefficients
+   * will be computed. Otherwise they will not be computed.
    */
-  void SetGain(int channel, double value)
+  void SetGain(int channel, double value, bool update = true) override
   {
     gain[channel] = value;
-    Setup();
+    if (update) {
+      Setup(channel);
+    }
   }
 
   /**
    * Sets the quality on a specific channel
    * @param channel the channel on which to change the quality
    * @param value the new quality
+   * @param update if true, as it is by default, the new filter coefficients
+   * will be computed. Otherwise they will not be computed.
    */
-  void SetQuality(int channel, double value)
+  void SetQuality(int channel, double value, bool update = true) override
   {
     quality[channel] = value;
-    Setup();
+    if (update) {
+      Setup(channel);
+    }
   }
 
   /**
    * Sets the filter type on a specific channel
    * @param channel the channel on which to change the filter type
    * @param value the new filter type
+   * @param update if true, as it is by default, the new filter coefficients
+   * will be computed. Otherwise they will not be computed.
    */
-  void SetBiquadFilterType(int channel, BiquadFilterType value)
+  void SetBiquadFilterType(int channel,
+                           BiquadFilterType value,
+                           bool update = true) override
   {
     filterType[channel] = value;
-    Setup();
+    if (update) {
+      Setup(channel);
+    }
   }
 
   /**
@@ -189,123 +246,169 @@ public:
    * @param channel the channel to get the cutoff frequency from
    * @return the cutoff frequency of the specified channel.
    */
-  double GetFrequency(int channel) const { return frequency[channel]; }
+  double GetFrequency(int channel) const override { return frequency[channel]; }
 
   /**
    * Gets the gain used for the specified channel.
    * @param channel the channel to get the gain from
    * @return the gain of the specified channel.
    */
-  double GetGain(int channel) const { return gain[channel]; }
+  double GetGain(int channel) const override { return gain[channel]; }
 
   /**
    * Gets the quality used for the specified channel.
    * @param channel the channel to get the quality from
    * @return the quality of the specified channel.
    */
-  double GetQuality(int channel) const { return quality[channel]; }
+  double GetQuality(int channel) const override { return quality[channel]; }
 
   /**
    * Gets the filter type used for the specified channel.
    * @param channel the channel to get the filter type from
    * @return the filter type of the specified channel.
    */
-  BiquadFilterType GetBiquadFilterType(int channel) const
+  BiquadFilterType GetBiquadFilterType(int channel) const override
   {
     return filterType[channel];
   }
 
-private:
-  void Setup()
+  /**
+   * Computes the filter coefficients for a specified channel.
+   * @param channel the channel to compute the coefficients the coeffcient for
+   * @param reset if true, as it is by default, resets the state of filter,
+   * calling Reset().
+   */
+  void Setup(int channel, bool reset = true) override
   {
-    for (int i = 0; i < Vec::size(); ++i) {
-      double a1 = 0.0;
-      double a2 = 0.0;
-      double b0 = 0.0;
-      double b1 = 0.0;
-      double b2 = 0.0;
-      double g = pow(10.0, gain[i] / 40.0);
-      double cs = cos(frequency[i]);
-      double sn = sin(frequency[i]);
-      double alpha = sn / (2.0 * quality[i]);
-      double sq = 2.0 * sqrt(g) * alpha;
-      switch (filterType[i]) {
-        case BiquadFilterType::HighShelf: {
-          double a0inv = 1.0 / ((g + 1.0) - (g - 1.0) * cs + sq);
-          b0 = a0inv * g * ((g + 1.0) + (g - 1.0) * cs + sq);
-          b1 = -2.0 * a0inv * g * ((g - 1.0) + (g + 1.0) * cs);
-          b2 = a0inv * g * ((g + 1.0) + (g - 1.0) * cs - sq);
-          a1 = 2.0 * a0inv * ((g - 1.0) - (g + 1.0) * cs);
-          a2 = a0inv * ((g + 1.0) - (g - 1.0) * cs - sq);
-        } break;
-        case BiquadFilterType::HighPass: {
-          double a0inv = 1.0 / (1.0 + alpha);
-          b0 = a0inv * 0.5 * (1.0 + cs);
-          b1 = a0inv * (-1.0 - cs);
-          b2 = a0inv * 0.5 * (1.0 + cs);
-          a1 = -2.0 * a0inv * cs;
-          a2 = (1.0 - alpha) * a0inv;
-        } break;
-        case BiquadFilterType::LowShelf: {
-          double a0inv = 1.0 / ((g + 1.0) + (g - 1.0) * cs + sq);
-          b0 = a0inv * g * ((g + 1.0) - (g - 1.0) * cs + sq);
-          b1 = 2.0 * a0inv * g * ((g - 1.0) - (g + 1.0) * cs);
-          b2 = a0inv * g * ((g + 1.0) - (g - 1.0) * cs - sq);
-          a1 = -2.0 * a0inv * ((g - 1.0) + (g + 1.0) * cs);
-          a2 = a0inv * ((g + 1.0) + (g - 1.0) * cs - sq);
-        } break;
-        case BiquadFilterType::LowPass: {
-          double a0inv = 1.0 / (1.0 + alpha);
-          b0 = a0inv * (1.0 - cs) * 0.5;
-          b1 = a0inv * (1.0 - cs);
-          b2 = a0inv * (1.0 - cs) * 0.5;
-          a1 = a0inv * (-2.0 * cs);
-          a2 = a0inv * (1.0 - alpha);
-        } break;
-        case BiquadFilterType::Peak: {
-          double a0inv = 1.0 / (1.0 + alpha / g);
-          b0 = a0inv * (1.0 + alpha * g);
-          b1 = a0inv * (-2.0 * cs);
-          b2 = a0inv * (1.0 - alpha * g);
-          a1 = a0inv * (-2.0 * cs);
-          a2 = a0inv * (1.0 - alpha / g);
-        } break;
-        case BiquadFilterType::BandPass: {
-          double a0inv = 1.0 / (1.0 + alpha);
-          b0 = a0inv * alpha;
-          b1 = 0.0;
-          b2 = -a0inv * alpha;
-          a1 = -2.0 * a0inv * cs;
-          a2 = a0inv * (1.0 - alpha);
-        } break;
-        case BiquadFilterType::Notch: {
-          double a0inv = 1.0 / (1.0 + alpha);
-          b0 = a0inv;
-          b1 = -2.0 * a0inv * cs;
-          b2 = a0inv;
-          a1 = -2.0 * a0inv * cs;
-          a2 = a0inv * (1.0 - alpha);
-        } break;
-        case BiquadFilterType::AllPass: {
-          double a0inv = 1.0 / (1.0 + alpha);
-          b0 = a0inv * (1.0 - alpha);
-          b1 = -2.0 * a0inv * cs;
-          b2 = a0inv * (1.0 + alpha);
-          a1 = -2.0 * a0inv * cs;
-          a2 = a0inv * (1.0 - alpha);
-        } break;
-        default:
-          assert(false);
-      }
-      buffer[0][i] = a1;
-      buffer[1][i] = a2;
-      buffer[2][i] = b0;
-      buffer[3][i] = b1;
-      buffer[4][i] = b2;
+    double a1 = 0.0;
+    double a2 = 0.0;
+    double b0 = 0.0;
+    double b1 = 0.0;
+    double b2 = 0.0;
+    double g = pow(10.0, gain[channel] / 40.0);
+    double cs = cos(frequency[channel]);
+    double sn = sin(frequency[channel]);
+    double alpha = sn / (2.0 * quality[channel]);
+    double sq = 2.0 * sqrt(g) * alpha;
+    switch (filterType[channel]) {
+      case BiquadFilterType::HighShelf: {
+        double a0inv = 1.0 / ((g + 1.0) - (g - 1.0) * cs + sq);
+        b0 = a0inv * g * ((g + 1.0) + (g - 1.0) * cs + sq);
+        b1 = -2.0 * a0inv * g * ((g - 1.0) + (g + 1.0) * cs);
+        b2 = a0inv * g * ((g + 1.0) + (g - 1.0) * cs - sq);
+        a1 = 2.0 * a0inv * ((g - 1.0) - (g + 1.0) * cs);
+        a2 = a0inv * ((g + 1.0) - (g - 1.0) * cs - sq);
+      } break;
+      case BiquadFilterType::HighPass: {
+        double a0inv = 1.0 / (1.0 + alpha);
+        b0 = a0inv * 0.5 * (1.0 + cs);
+        b1 = a0inv * (-1.0 - cs);
+        b2 = a0inv * 0.5 * (1.0 + cs);
+        a1 = -2.0 * a0inv * cs;
+        a2 = (1.0 - alpha) * a0inv;
+      } break;
+      case BiquadFilterType::LowShelf: {
+        double a0inv = 1.0 / ((g + 1.0) + (g - 1.0) * cs + sq);
+        b0 = a0inv * g * ((g + 1.0) - (g - 1.0) * cs + sq);
+        b1 = 2.0 * a0inv * g * ((g - 1.0) - (g + 1.0) * cs);
+        b2 = a0inv * g * ((g + 1.0) - (g - 1.0) * cs - sq);
+        a1 = -2.0 * a0inv * ((g - 1.0) + (g + 1.0) * cs);
+        a2 = a0inv * ((g + 1.0) + (g - 1.0) * cs - sq);
+      } break;
+      case BiquadFilterType::LowPass: {
+        double a0inv = 1.0 / (1.0 + alpha);
+        b0 = a0inv * (1.0 - cs) * 0.5;
+        b1 = a0inv * (1.0 - cs);
+        b2 = a0inv * (1.0 - cs) * 0.5;
+        a1 = a0inv * (-2.0 * cs);
+        a2 = a0inv * (1.0 - alpha);
+      } break;
+      case BiquadFilterType::Peak: {
+        double a0inv = 1.0 / (1.0 + alpha / g);
+        b0 = a0inv * (1.0 + alpha * g);
+        b1 = a0inv * (-2.0 * cs);
+        b2 = a0inv * (1.0 - alpha * g);
+        a1 = a0inv * (-2.0 * cs);
+        a2 = a0inv * (1.0 - alpha / g);
+      } break;
+      case BiquadFilterType::BandPass: {
+        double a0inv = 1.0 / (1.0 + alpha);
+        b0 = a0inv * alpha;
+        b1 = 0.0;
+        b2 = -a0inv * alpha;
+        a1 = -2.0 * a0inv * cs;
+        a2 = a0inv * (1.0 - alpha);
+      } break;
+      case BiquadFilterType::Notch: {
+        double a0inv = 1.0 / (1.0 + alpha);
+        b0 = a0inv;
+        b1 = -2.0 * a0inv * cs;
+        b2 = a0inv;
+        a1 = -2.0 * a0inv * cs;
+        a2 = a0inv * (1.0 - alpha);
+      } break;
+      case BiquadFilterType::AllPass: {
+        double a0inv = 1.0 / (1.0 + alpha);
+        b0 = a0inv * (1.0 - alpha);
+        b1 = -2.0 * a0inv * cs;
+        b2 = a0inv * (1.0 + alpha);
+        a1 = -2.0 * a0inv * cs;
+        a2 = a0inv * (1.0 - alpha);
+      } break;
+      default:
+        assert(false);
     }
-    Reset();
+    buffer[0][channel] = a1;
+    buffer[1][channel] = a2;
+    buffer[2][channel] = b0;
+    buffer[3][channel] = b1;
+    buffer[4][channel] = b2;
+
+    if (reset) {
+      Reset(channel);
+    }
   }
 
+  /**
+   * Computes the filter coefficients.
+   * @param reset if true, as it is by default, resets the state of filter,
+   * calling Reset().
+   */
+  void Setup(bool reset = true)
+  {
+    for (int i = 0; i < Vec::size(); ++i) {
+      Setup(i, false);
+    }
+    if (reset) {
+      Reset();
+    }
+  }
+
+  /**
+   * Sets the state of the filter for a specified channel.
+   * @param channel the channel for which to set the state of the filter
+   * @param state0 the first number of the state of the filter
+   * @param state1 the second number of the state of the filter
+   */
+  void SetState(int channel, Scalar state0, Scalar state1) override
+  {
+    buffer[5][channel] = state0;
+    buffer[6][channel] = state1;
+  }
+
+  /**
+   * Gets the state of the filter for a specified channel.
+   * @param channel the channel for which to set the state of the filter
+   * @param state0 the first number of the state of the filter
+   * @param state1 the second number of the state of the filter
+   */
+  void GetState(int channel, Scalar& state0, Scalar& state1) const override
+  {
+    state0 = buffer[5][channel];
+    state1 = buffer[6][channel];
+  }
+
+private:
   VecBuffer<Vec> buffer;
   std::array<double, Vec::size()> frequency;
   std::array<double, Vec::size()> quality;
@@ -779,6 +882,181 @@ public:
    * @return the maximum number of channel that the generator can work with.
    */
   int GetNumChannels() const { return numChannels; }
+
+  /**
+   * Resets the state of the filter for a sequence of channels.
+   * @param srcChannelStart the first channel to be reset
+   * @param numChannelsToReset the number of channels to reset
+   */
+  void Reset(int srcChannelStart, int numChannelsToReset)
+  {
+    for (int i = srcChannelStart; i < srcChannelStart + numChannelsToReset;
+         ++i) {
+
+      if constexpr (VEC8_AVAILABLE) {
+        auto d8 = std::div(i, 8);
+
+#if AVEC_MIX_VEC_SIZES
+
+        VecBiquadFilterInterface<Scalar>* filter =
+          d8.quot < filters8.size()
+            ? static_cast<VecBiquadFilterInterface<Scalar>*>(&filters8[d8.quot])
+            : static_cast<VecBiquadFilterInterface<Scalar>*>(&filters4[0]);
+
+#else
+
+        VecBiquadFilter<Vec8>* filter = &filters8[d8.quot];
+
+#endif
+
+        filter->Reset(d8.rem);
+      }
+      else if constexpr (VEC4_AVAILABLE) {
+        auto d4 = std::div(i, 4);
+        filters4[d4.quot].Reset(d4.rem);
+      }
+      else {
+        auto d2 = std::div(i, 2);
+        filters2[d4.quot].Reset(d2.rem);
+      }
+    }
+  }
+
+  /**
+   * Copies the state and settings of the filter from sequence of channels to
+   * an other sequence of channels, and resets the latter.
+   * @param srcChannelStart the first channel to be moved
+   * @param dstChannelStart the destination for the first channel to be moved
+   * @param numChannelsToMove the number of channels to move
+   */
+  void MoveChannelStates(int srcChannelStart,
+                         int dstChannelStart,
+                         int numChannelsToMove)
+  {
+    double srcFrequency;
+    double srcQuality;
+    double srcGain;
+    BiquadFilterType srcType;
+
+    Scalar srcState0;
+    Scalar srcState1;
+
+    for (int i = 0; i < numChannelsToMove; ++i) {
+      int srcChannel = srcChannelStart + i;
+      int dstChannel = dstChannelStart + i;
+
+      if constexpr (VEC8_AVAILABLE) {
+        auto s8 = std::div(srcChannel, 8);
+        auto d8 = std::div(dstChannel, 8);
+
+#if AVEC_MIX_VEC_SIZES
+
+        VecBiquadFilterInterface<Scalar>* srcFilter =
+          s8.quot < filters8.size()
+            ? static_cast<VecBiquadFilterInterface<Scalar>*>(&filters8[s8.quot])
+            : static_cast<VecBiquadFilterInterface<Scalar>*>(&filters4[0]);
+        VecBiquadFilterInterface<Scalar>* dstFilter =
+          d8.quot < filters8.size()
+            ? static_cast<VecBiquadFilterInterface<Scalar>*>(&filters8[d8.quot])
+            : static_cast<VecBiquadFilterInterface<Scalar>*>(&filters4[0]);
+
+#else
+
+        VecBiquadFilter<Vec8>* srcFilter = &filters8[s8.quot];
+        VecBiquadFilter<Vec8>* dstFilter = &filters8[d8.quot];
+
+#endif
+
+        int src = s8.rem;
+        int dst = d8.rem;
+
+        srcFrequency = srcFilter->GetFrequency(src);
+        srcQuality = srcFilter->GetQuality(src);
+        srcGain = srcFilter->GetGain(src);
+        srcType = srcFilter->GetBiquadFilterType(src);
+
+        bool needsSetup = (srcFrequency != dstFilter->GetFrequency(dst)) ||
+                          (srcQuality != dstFilter->GetQuality(dst)) ||
+                          (srcGain != dstFilter->GetGain(dst)) ||
+                          (srcType != dstFilter->GetBiquadFilterType(dst));
+
+        dstFilter->SetFrequency(dst, srcFrequency, false);
+        dstFilter->SetQuality(dst, srcQuality, false);
+        dstFilter->SetGain(dst, srcGain, false);
+        dstFilter->SetBiquadFilterType(dst, srcType, false);
+
+        if (needsSetup) {
+          dstFilter->Setup(dst, false);
+        }
+
+        srcFilter->GetState(src, srcState0, srcState1);
+        dstFilter->SetState(dst, srcState0, srcState1);
+        srcFilter->Reset(src);
+      }
+      else if constexpr (VEC4_AVAILABLE) {
+        auto s4 = std::div(src, 4);
+        auto d4 = std::div(dst, 4);
+        auto& srcFilter = filters4[s4.quot];
+        auto& dstFilter = filters4[d4.quot];
+        int src = s4.rem;
+        int dst = d4.rem;
+
+        srcFrequency = srcFilter.GetFrequency(src);
+        srcQuality = srcFilter.GetQuality(src);
+        srcGain = srcFilter.GetGain(src);
+        srcType = srcFilter.GetBiquadFilterType(src);
+
+        bool needsSetup = (srcFrequency != dstFilter.GetFrequency(dst)) ||
+                          (srcQuality != dstFilter.GetQuality(dst)) ||
+                          (srcGain != dstFilter.GetGain(dst)) ||
+                          (srcType != dstFilter.GetBiquadFilterType(dst));
+
+        dstFilter.SetFrequency(dst, srcFrequency, false);
+        dstFilter.SetQuality(dst, srcQuality, false);
+        dstFilter.SetGain(dst, srcGain, false);
+        dstFilter.SetBiquadFilterType(dst, srcType, false);
+
+        if (needsSetup) {
+          dstFilter.Setup(dst, false);
+        }
+
+        srcFilter.GetState(src, srcState0, srcState1);
+        dstFilter.SetState(dst, srcState0, srcState1);
+        srcFilter.Reset(src);
+      }
+      else {
+        auto s2 = std::div(src, 2);
+        auto d2 = std::div(dst, 2);
+        auto& srcFilter = filters2[s2.quot];
+        auto& dstFilter = filters2[d2.quot];
+        int src = s2.rem;
+        int dst = d2.rem;
+
+        srcFrequency = srcFilter.GetFrequency(src);
+        srcQuality = srcFilter.GetQuality(src);
+        srcGain = srcFilter.GetGain(src);
+        srcType = srcFilter.GetBiquadFilterType(src);
+
+        bool needsSetup = (srcFrequency != dstFilter.GetFrequency(dst)) ||
+                          (srcQuality != dstFilter.GetQuality(dst)) ||
+                          (srcGain != dstFilter.GetGain(dst)) ||
+                          (srcType != dstFilter.GetBiquadFilterType(dst));
+
+        dstFilter.SetFrequency(dst, srcFrequency, false);
+        dstFilter.SetQuality(dst, srcQuality, false);
+        dstFilter.SetGain(dst, srcGain, false);
+        dstFilter.SetBiquadFilterType(dst, srcType, false);
+
+        if (needsSetup) {
+          dstFilter.Setup(dst, false);
+        }
+
+        srcFilter.GetState(src, srcState0, srcState1);
+        dstFilter.SetState(dst, srcState0, srcState1);
+        srcFilter.Reset(src);
+      }
+    }
+  }
 };
 
 } // namespace avec
